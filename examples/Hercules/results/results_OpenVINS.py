75f9bf3972c86054d6dba1bfb5b4ed3ff449eac4
Daniel Butterfield <danielcbutter@gmail.com>
2026-01-08 18:17:04 -0500

    Results file for OpenVINS

--- examples/Hercules/results/results_OpenVINS.py
+++ examples/Hercules/results/results_OpenVINS.py
@@ -1,31 +1,42 @@
 from decimal import Decimal
+import numpy as np
 from robotdataprocess.data_types.OdometryData import OdometryData, CoordinateFrame
 from pprint import pprint
+from scipy.spatial.transform import Rotation as R
 
 def main():
     # Load the GT and estimated path data
-    # robot_names = ["Husky1", "Husky2", "Drone1", "Drone2"]
+    robot_names = ["Husky1", "Husky2", "Drone1", "Drone2"]
     dataset_version = "V2.1.0"
-    # file_names = ['vins_result_no_loop_reformatted.csv', 'vins_result_loop_reformatted.csv']
     file_name = 'ov_estimate.txt'
 
-    # for robot_name in robot_names:
-    #     for file_name in file_names:
+    for robot_name in robot_names:
+        # Load Estimated data from OpenVINS
+        est_data = OdometryData.from_txt_file("/media/dbutterfield3/T73/Hercules_datasets/" + dataset_version 
+                                              + "/results/openvins/" + robot_name+ '/' + file_name, "world", "robot", 
+                                              CoordinateFrame.FLU, True, [0, 5, 6, 7, 4, 1, 2, 3])
 
-    robot_folder = "/media/dbutterfield3/T73/Hercules_datasets/" + dataset_version + "/results/openvins"
-    est_data = OdometryData.from_txt_file(robot_folder + '/' + file_name, "world", "robot", CoordinateFrame.FLU, True, [0, 5, 6, 7, 4, 1, 2, 3])
-    est_data.visualize([], ["OpenVINS EuRoC"], axes_interval=50, axes_length=0.3)
-    # gt_data = OdometryData.from_csv(robot_folder +'/poseGT.csv', 
-    #                                 "world", "robot", CoordinateFrame.FLU, True, None)
+        # Orientations are in NED rotated to FLU, lets fix that, but should be change of basis instead
+        R_NED = np.array([[1,  0,  0],
+                            [0, -1,  0],
+                            [0,  0, -1]])
+        R_NED_Q = R.from_matrix(R_NED)
+        est_data._ori_apply_rotation(R_NED_Q.inv())
+        est_data._ori_change_of_basis(R_NED_Q)
 
-    # Calculate RMS ATE, among other metrics
-    # metrics_dictionary: dict = OdometryData.calculate_trajectory_errors(gt_data, est_data, max_diff=0.1, visualize=True)
-    # print("\nMetrics for file: ", file_name)
-    # print("Robot: ", robot_name, "RMS ATE: ", metrics_dictionary['APE']['translation_part']['rmse'])
-    # print("Robot: ", robot_name, "RMS RTE: ", metrics_dictionary['RPE']['translation_part']['rmse'])
+        # Load the GT Data
+        gt_data = OdometryData.from_csv("/media/dbutterfield3/T73/Hercules_datasets/" + dataset_version + \
+                                        "/extract/files_for_roman_baseline/" + robot_name +'/poseGT.csv', 
+                                        "world", "robot", CoordinateFrame.FLU, True, None)
 
-    # print("Robot: ", robot_name, "RMS APE Rotation Angle (Deg): ", metrics_dictionary['APE']['rotation_angle_deg']['rmse'])
-    # print("Robot: ", robot_name, "RMS RTE Rotation Angle (Deg): ", metrics_dictionary['RPE']['rotation_angle_deg']['rmse'])
+        # Calculate RMS ATE, among other metrics
+        metrics_dictionary: dict = OdometryData.calculate_trajectory_errors(gt_data, est_data, max_diff=0.1, visualize=True)
+        print("\nMetrics for file: ", file_name)
+        print("Robot: ", robot_name, "RMS ATE: ", metrics_dictionary['APE']['translation_part']['rmse'])
+        print("Robot: ", robot_name, "RMS RTE: ", metrics_dictionary['RPE']['translation_part']['rmse'])
+
+        print("Robot: ", robot_name, "RMS APE Rotation Angle (Deg): ", metrics_dictionary['APE']['rotation_angle_deg']['rmse'])
+        print("Robot: ", robot_name, "RMS RTE Rotation Angle (Deg): ", metrics_dictionary['RPE']['rotation_angle_deg']['rmse'])
 
 if __name__ == "__main__":
     main()
