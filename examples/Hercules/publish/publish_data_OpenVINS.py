feec1b99d56c916b300a48e7aa0cc70c3bbf0d48
Daniel Butterfield <danielcbutter@gmail.com>
2026-02-06 10:54:55 -0500

    Cropping for HERCULES dataset with OpenVINS

--- examples/Hercules/publish/publish_data_OpenVINS.py
+++ examples/Hercules/publish/publish_data_OpenVINS.py
@@ -8,11 +8,7 @@ from robotdataprocess.data_types.ImageData.ImageDataOnDisk import ImageDataOnDis
 from robotdataprocess.ros.RosPublisher import publish_data_ROS_multiprocess
 from typing import Union
 
-def publish_data(input_dir: str, robot_name: str, crop_data: bool, start_time: Decimal, end_time: Union[Decimal, None]):
-    # Check parameters
-    if crop_data and start_time is None:
-        raise ValueError("start_time required if crop_data is True!")
-    
+def publish_data(input_dir: str, robot_name: str, start_time: Decimal, end_time: Union[Decimal, None]):
     # Extract data from Hercules
     input_path = Path(input_dir).absolute() 
     imu_data = ImuData.from_txt_file(input_path / robot_name / 'synthetic_imu_9axis_500Hz.txt', '' + robot_name + '/base_link', CoordinateFrame.NED, True)
@@ -25,11 +21,10 @@ def publish_data(input_dir: str, robot_name: str, crop_data: bool, start_time: D
     pose_data.shift_to_start_at_identity()
 
     # Crop the data
-    if crop_data:
-        imu_data.crop_data(start_time, end_time)
-        pose_data.crop_data(start_time, end_time)
-        left_image_data.crop_data(start_time, end_time)
-        right_image_data.crop_data(start_time, end_time)
+    imu_data.crop_data(start_time, end_time)
+    pose_data.crop_data(start_time, end_time)
+    left_image_data.crop_data(start_time, end_time)
+    right_image_data.crop_data(start_time, end_time)
 
     # Publish the data via ROS2 topics 
     publish_data_ROS_multiprocess([imu_data, pose_data, left_image_data, right_image_data], 
@@ -39,30 +34,46 @@ def publish_data(input_dir: str, robot_name: str, crop_data: bool, start_time: D
                                   [1, 1, 3, 3],
                                    ROSMsgLibType.RCLPY, True, verbose=True)
     
-def main(dataset_num: str, robot_name: str, crop_start_time: Union[float, None], crop_end_time: Union[float, None]): 
+def main(dataset_num: str, robot_name: str, crop_start_time: float, crop_end_time: Union[float, None]): 
 
     # Do bookkeeping for cropping
-    if crop_end_time == None and crop_start_time == None: 
-        crop_data = False
-    else: 
-        crop_data = True
-        crop_start_time = Decimal(crop_start_time) if crop_start_time is not None else Decimal('0.0')
-        crop_end_time = Decimal(crop_end_time) if crop_end_time is not None else None
+    crop_start_time = Decimal(crop_start_time)
+    crop_end_time = Decimal(crop_end_time) if crop_end_time is not None else None
     
     # Publish the data for the specified robot
     user = getpass.getuser()
     publish_data(input_dir='/home/' + user + '/data/Hercules_datasets/' + dataset_num + '/data',
                     robot_name=robot_name,
-                    crop_data=crop_data,
                     start_time=crop_start_time,
                     end_time=crop_end_time)
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Publish Hercules data via ROS2 topics for VINS-Mono.")
-    parser.add_argument('--dataset_num', type=str, default=None, help="Dataset version number (e.g., V1.6).")
+    parser.add_argument('--dataset_num', type=str, default=None, help="Dataset version number.")
     parser.add_argument('--robot_name', type=str, default=None, help="Name of the robot (e.g., Drone1).")
-    parser.add_argument('--crop_start_time', type=float, default=None, help="Optional start time (in seconds) to crop the data.")
-    parser.add_argument('--crop_end_time', type=float, default=None, help="Optional end time (in seconds) to crop the data.")
     args = parser.parse_args()
 
-    main(dataset_num=args.dataset_num, robot_name=args.robot_name, crop_start_time=args.crop_start_time, crop_end_time=args.crop_end_time)
+    # OpenVINS starts from zero as it desires static initialization
+    if args.dataset_num == "V2.4.C":
+        robot_crop_start_times = [Decimal('0.0'), Decimal('0.0'), Decimal('0.0'), Decimal('0.0')]
+        robot_crop_end_times = [Decimal('382.85'), Decimal('390.90'), Decimal('1100.00'), Decimal('1190.35')]
+    elif args.dataset_num == "V2.3.AP":
+        robot_crop_start_times = [Decimal('0.0'), Decimal('0.0'), Decimal('0.0'), Decimal('0.0')]
+        robot_crop_end_times = [Decimal('772.15'), Decimal('741.45'), Decimal('1121.80'), Decimal('1193.80')]
+    elif args.dataset_num == "V2.3.AC":
+        robot_crop_start_times = [Decimal('0.0'), Decimal('0.0'), Decimal('0.0'), Decimal('0.0')]
+        robot_crop_end_times = [Decimal('1125.00'), Decimal('1118.80'), Decimal('1025.50'), Decimal('892.60')]
+    else:
+        raise ValueError("Crop times not specified for this dataset number.")
+    
+    robot_names = ["Husky1", "Husky2", "Drone1", "Drone2"]
+
+    # Get start and end times for the specified robot
+    if args.robot_name not in robot_names:
+        raise ValueError(f"Invalid robot name. Must be one of {robot_names}.")
+    robot_index = robot_names.index(args.robot_name)
+    crop_start_time = robot_crop_start_times[robot_index]
+    crop_end_time = robot_crop_end_times[robot_index]
+
+
+    main(dataset_num=args.dataset_num, robot_name=args.robot_name, crop_start_time=crop_start_time, crop_end_time=crop_end_time)
