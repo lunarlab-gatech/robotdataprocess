59831d177247342f405b0ea7f9bb8b1b576a1bd3
Daniel Butterfield <danielcbutter@gmail.com>
2026-03-03 17:52:11 -0500

    ReasoningGraph now successfully builds a Metric-Semantic Mesh off HERCULES

--- examples/Meronomy/publish/publish_data_ReasoningGraph.py
+++ examples/Meronomy/publish/publish_data_ReasoningGraph.py
@@ -2,7 +2,7 @@ import argparse
 from decimal import Decimal
 import getpass
 from pathlib import Path
-from robotdataprocess import ImuData, OdometryData, CoordinateFrame, LiDARData, CameraData, TransformationData
+from robotdataprocess import OdometryData, CoordinateFrame, CameraData, TransformationData, TransformType
 from robotdataprocess.data_types.Data import ROSMsgLibType
 from robotdataprocess.data_types.ImageData.ImageDataOnDisk import ImageDataOnDisk
 from robotdataprocess.ros.RosPublisher import publish_data_ROS_multiprocess
@@ -21,24 +21,34 @@ def publish_data(input_dir: str, robot_name: str):
     odom_data.apply_transformation_right_side(H_B_to_L.invert().as_matrix())
     odom_data.to_coordinate_frame(CoordinateFrame.FLU)
 
+    # Extract Robot to Camera transformation
+    H_R_to_C = TransformationData.from_HERCULES_settings_json(str(input_path / 'settings.json'), robot_name, "camera", "stereo_left")
+    H_C_to_O = TransformationData.optical_wrt_camera(CoordinateFrame.NED, "stereo_left", "stereo_left_optical")
+    H_R_to_O = H_R_to_C.apply_transformation_right_side(H_C_to_O)
+    H_R_to_O_in_FLU = H_R_to_O.to_coordinate_frame(CoordinateFrame.FLU, TransformType.ROTATION)
+    H_R_to_O_in_FLU.frame_id = "body"
+    H_R_to_O_in_FLU.child_frame_id = 'camera_color_optical_frame'
+
     # Define Camera parameters
     left_camera_data = CameraData.from_user_mono('/cam0', 752, 480, 376, 376, 376, 240, CameraData.DistortionModel.RADIAL_TANGENTIAL)
+    left_camera_data.sync_to_ImageData(left_image_data)
 
-    # Multiply all timestamps by 20 to simulate traveling 20 times slower
-    for data in [left_image_data, depth_data, left_camera_data, left_camera_data, odom_data]:
-        data.timestamps = data.timestamps * Decimal('20.0')
+    # Multiply all timestamps by 5 to simulate traveling 5 times slower
+    for data in [left_image_data, depth_data, left_camera_data, odom_data]:
+        data.timestamps = data.timestamps * Decimal('5.0')
     odom_data.interpolate_to_hz(4)
 
     # Publish the data via ROS topics
-    publish_data_ROS_multiprocess([left_image_data, depth_data, left_camera_data, left_camera_data, odom_data], 
-                                  ['/'+robot_name+'/cam0/image_raw',
-                                   '/'+robot_name+'/cam0/depth/image_raw',
-                                   '/'+robot_name+'/cam0/camera_info',
-                                   '/'+robot_name+'/cam0/depth/camera_info',
-                                   '/tf'],
-                                    [None, None, None, None, "TFMessage"], 
-                                    [20, 20, 1, 1, 20],
-                                    [2, 2, 1, 1, 1], ROSMsgLibType.ROSPY, True, verbose=True)
+    publish_data_ROS_multiprocess([left_image_data, depth_data, left_camera_data, left_camera_data, odom_data, H_R_to_O_in_FLU], 
+                                  ['/robot/cam0/image_raw',
+                                   '/robot/cam0/depth/image_raw',
+                                   '/robot/cam0/camera_info',
+                                   '/robot/cam0/depth/camera_info',
+                                   '/tf', 
+                                   '/tf_static'],
+                                    [None, None, None, None, "TFMessage", None], 
+                                    [20, 20, 20, 20, 20, 1],
+                                    [2, 2, 1, 1, 1, 1], ROSMsgLibType.ROSPY, True, verbose=True, publish_clock=False)
 
 def main(dataset_num: str, robot_name: str): 
     user = getpass.getuser()
